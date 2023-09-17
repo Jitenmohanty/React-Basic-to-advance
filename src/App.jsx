7e10c14a688f433f5ebaa89909b34f7e568069f5
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2023-09-17 22:06:02 +0530

    Add reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import "./App.css";
 import Video from "./Components/Video";
 import data from "./assets/data.json";
@@ -6,32 +6,46 @@ import data from "./assets/data.json";
 import AddVideo from "./Components/AddVideo";
 
 function App() {
-  const [videos, setVideos] = useState(data.projects);
+  function videoReducer(videos, action) {
+    switch (action.type) {
+      case "ADD":
+        return [
+          ...videos,
+          {
+            ...action.payload,
+            id: Date.now(),
+            imgSrc:
+              "https://images.freeimages.com/images/large-previews/155/bridge-1559052.jpg?fmt=webp&w=350",
+          },
+        ];
+      case "DELETE":
+        return videos.filter((video) => video.id !== action.payload);
+      case "UPDATE":
+        const index = videos.findIndex((v) => v.id === action.payload.id);
+        const uVideo = [...videos];
+        uVideo.splice(index, 1, action.payload);
+        return uVideo;
+
+      default:
+        return videos;
+    }
+  }
+
+  const [videos, dispatch] = useReducer(videoReducer, data.projects);
+
   const [editableVideo, setEditableVideo] = useState(null);
   const addVideo = (video) => {
-    setVideos([
-      ...videos,
-      {
-        ...video,
-        id: Date.now(),
-        imgSrc:
-          "https://images.freeimages.com/images/large-previews/155/bridge-1559052.jpg?fmt=webp&w=350",
-      },
-    ]);
+    //Total  called action
+    dispatch({ type: "ADD", payload: video });
   };
   const deleteVideo = (id) => {
-    setVideos(videos.filter((video) => video.id !== id));
+    dispatch({ type: "DELETE", payload: id });
   };
   const editVideo = (id) => {
     setEditableVideo(videos.find((video) => video.id === id));
   };
   const updateVideo = (video) => {
-    const index = videos.findIndex(v=> v.id === video.id)
-    const uVideo = [...videos];
-    uVideo.splice(index,1,video)
-      setVideos(uVideo)
-    // console.log(uVideo)
-    // console.log(video);
+    dispatch({ type: "UPDATE", payload: video });
   };
 
   return (
